947b6d7c9bb8e1883181a036f6cfb39b3ab2213d
cendrars59 <cyr.leroux.git@moinslequart.net>
2019-10-23 21:58:54 +0200

    Not optimized solution for palindrom. But it works :-)

--- palindrom.py
+++ palindrom.py
@@ -1,30 +1,37 @@
 import re
 
-exclusionList = [("é|è|ê",'e'),(",|!|:|'",""),("ç","c"),("à|â","a")]
+exclusion_list = [("é|è|ê", 'e'), ("-|.|,|!|:|'| ", ""), ("ç", "c"), ("À|à|â", "a")]
 
 
-def clean_Word(value,exclusionList):
-        for reg in exclusionList:
-                return re.sub(reg[0],reg[1],value)
+def clean_string(value, exclusion_list_p):
+    transform = []
+    for letter in value:
+        for rule in exclusion_list_p:
+            if letter in rule[0]:
+                new_value = re.sub(rule[0], rule[1], letter)
+                break
+            else:
+                new_value = letter
+        transform.append(new_value.lower())
+        if "" in transform:
+            transform.remove("")
+    return transform
 
 
+def is_palindrome(thestring):
+    string_to_evaluate = clean_string(thestring, exclusion_list)
+    if string_to_evaluate == string_to_evaluate[::-1] and len(thestring) != 0:
+        print("{0} is a palindrome".format(thestring))
+    else:
+        print("{0} is not a palindrome".format(thestring))
 
-
-def is_palindrome(theString):
-        stringToEvaluate=clean_Word(theString, exlusionList).lower()
-
-        if stringToEvaluate == stringToEvaluate[::-1] and len(theString) !=0:
-                print("{0} is a palindrome".format(theString))
-        else: 
-                print("{0} is not a palindrome".format(theString))
 
 def main():
+    names_list = ['anna', 'çyril', 'Boob!', 'sébastien', '', '11', '112', '121', '@!@!@',
+                  'Dans la Prairie verte, il y a une bête !', 'À Cuba, Anna a bu ça.', "À l'étape, épate-la ! "]
 
-        names_list = ['anna','cyril','Boob!', 'sebastien','','11','112','121','@!@!@']
-    
-        for name in names_list:
-                is_palindrome(name)
+    for name in names_list:
+        is_palindrome(name)
+
 
 main()
-
-
